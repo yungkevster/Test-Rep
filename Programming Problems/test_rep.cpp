71989684e74db2da8463da2a66bc832dc7abfcd5
yungkevster <leuncrafter@gmail.com>
2022-11-20 18:38:46 +0100

    lol

--- Programming Problems/test_rep.cpp
+++ Programming Problems/test_rep.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <string>
 
-int main()
+struct World_List_Type
 {
-    std::cout << "Test" << std::endl;
+  vector<string>;
+};
+
+int main(){
+    
+  Word_List_Type words, sequential_words;
+
+  cout << "Enter words:" << endl;
+  read_words(words);
+
+  find_words_with_sequential_letters(words, sequential_words);
+
+  cout << "Words containing sequential letters:" << endl;
+  print(sequential_words);
+  
+  cout << "End of program.";
 
     return 0;
 }
